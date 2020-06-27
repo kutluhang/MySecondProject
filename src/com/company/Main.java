1f9b18b9bafd9f77da993e7f940c96b3832ca94d
kutluhang <kutluhang@yahoo.com>
2020-06-27 03:03:22 +0200

    MySecondProject Initialization
    Initialization

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+        float f = (float) 4.5;
+        double d = 3.5;
+        d = 3;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
